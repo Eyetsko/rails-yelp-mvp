ab15816399e4a7ff7dc3f27beab769aa3fd015da
Emily Yetsko <emilyyetsko@gmail.com>
2023-05-23 02:18:54 +0200

    solve yelp mvp

--- Gemfile
+++ Gemfile
@@ -27,6 +27,10 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+gem "simple_form"
+
+gem "faker"
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
@@ -61,6 +65,5 @@ group :development do
   # gem "spring"
 end
 
-gem 'rspec-rails', group: [ :test ]
 gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#frozen_string_literal: true
+
+#this is a comment
+
+Restaurant.destroy_all
+
+puts "populating db.."
+
+5.times do
+  new_restaurant = Restaurant.new(name: Faker::TvShows::RuPaul.queen,
+                                  address: Faker::Address.full_address,
+                                  phone_number: Faker::PhoneNumber.cell_phone,
+                                  category: ['italian', 'french', 'japanese', 'belgian', 'chinese'].sample)
+
+  if new_restaurant.save
+    puts "#{new_restaurant.id} was created.."
+  end
+end
